9b9ad5d42337776590b6e6e2424cb3aec4ba850f
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-07-15 10:59:28 +0200

    Commento sul futuro

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -15,4 +15,9 @@ class Project extends Model
         "group",
         "date",
     ];
+    //Dipendenze presumo, da controllare
+    // public function types()
+    // {
+    //     return $this->belongsTo(Type::class);
+    // }
 }
