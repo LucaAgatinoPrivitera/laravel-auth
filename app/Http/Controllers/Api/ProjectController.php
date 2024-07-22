7be92d585e650fa36ea19016a2bf4d14af99b521
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-07-22 11:37:04 +0200

    Non funziona ancora, serve l'if

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -20,6 +20,7 @@ class ProjectController extends Controller
     public function show($id)
     {
             $project = Project::with('type')->where('id', $id)->get()->first();
+            // DA AGGIORNARE
             dd($project);
             // grazie a with type aggiungo la tabella della relazione
     }
