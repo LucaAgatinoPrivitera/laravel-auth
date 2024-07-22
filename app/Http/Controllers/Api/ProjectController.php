4e56de23d1b0f664703ba1649fe36383b304f3be
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-07-22 12:09:28 +0200

    da completare l'else

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -13,16 +13,26 @@ class ProjectController extends Controller
         return response()->json([
             'status' => 'success',
             // Il paginate mi indica quanti risultati avrò per pagina
-            'projects'=> Project::with('type')->orderByDesc('id')->paginate(2)
+            'projects' => Project::with('type')->orderByDesc('id')->paginate(2)
             // grazie a with type aggiungo la tabella della relazione
         ]);
     }
     public function show($id)
     {
-            $project = Project::with('type')->where('id', $id)->get()->first();
-            // DA AGGIORNARE
-            dd($project);
-            // grazie a with type aggiungo la tabella della relazione
+        $project = Project::with('type')->where('id', $id)->get()->first();
+        // DA AGGIORNARE
+        dd($project);
+        // grazie a with type aggiungo la tabella della relazione
+
+        if ($project) {
+            return response()->json([
+                'status' => 'success',
+                // Il paginate mi indica quanti risultati avrò per pagina
+                'project' => Project::with('type')->orderByDesc('id')->paginate(2)
+                // grazie a with type aggiungo la tabella della relazione
+            ]);
+        } else {
+            
+        }
     }
 }
-
